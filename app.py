9f46014386913c0323d3d683ae2d656a236c1c28
Tanvirazad3449 <41642034+Tanvirazad3449@users.noreply.github.com>
2022-02-25 18:52:29 +0600

    done

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask,request, url_for, redirect, render_template, jsonify
 import pandas as pd
 import pickle
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import make_column_transformer
-from sklearn.pipeline import make_pipeline
 
 pr_model = pickle.load(open("karworth_lr_model.pkl", "rb"))
 df = pd.read_csv("Karworth_Clean_Dataframe.csv")
@@ -30,34 +27,12 @@ def predict():
     engineSize = request.form.get('engineSize')
     mpg = request.form.get('mpg')
     mileage = request.form.get('mileage')
-
-    # ohe = OneHotEncoder()
-    # cols = ['brand','model','year', 'mileage', 'mpg', 'engineSize', 'transmission','fuelType']
-    # data_unseen = pd.DataFrame([[brand,model,year,fuelType,transmission,engineSize,mpg,mileage]], columns = ['brand','model','year', 'mileage', 'mpg', 'engineSize', 'transmission','fuelType'])
-    # oheFit = ohe.fit(data_unseen[cols])
-    # oheFit = oheFit.transform(data_unseen[cols])
-
-    # print("oheFit")
-
-    # print(oheFit)
     fixed_model = model.lstrip()
     predTest = pd.DataFrame(data=np.array([brand, fixed_model, year, transmission, mileage, fuelType, mpg, engineSize]).reshape(1,8),
                         columns = ['brand','model','year','transmission','mileage','fuelType', 'mpg', 'engineSize' ])
-
     pr_result = pr_model.predict(predTest)
     pr_result = "{:,}".format(int(pr_result[0]))
-
     print(pr_result)
-
-
-
-
-
-    
-    # final = np.array([brand,model,year,fuelType,transmission,engineSize,mpg,mileage])
-    # data_unseen = pd.DataFrame([final], columns = ['brand','model','year', 'mileage', 'mpg', 'engineSize', 'transmission','fuelType'])                         
-    
-    # prediction = pr_model.predict(data_unseen)
     return pr_result
 
 if __name__ == "__main__":
